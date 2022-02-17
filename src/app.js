40527c3336849f8d7b32596622041566a2b70d4f
Mauricio Espinosa <ing_mauricioespinosa@hotmail.com>
2022-02-17 15:30:23 -0600

    DockerReady

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import { __dirname } from './utils.js';
 
 const app = express();
 const PORT = process.env.PORT||8081;
-const server = app.listen(8080,()=>console.log(`Listening on PORT ${PORT}`))
+const server = app.listen(PORT,()=>console.log(`Listening on PORT ${PORT}`))
 app.use(express.static(__dirname+'/public'))
 
 app.get('/info',(req,res)=>{
